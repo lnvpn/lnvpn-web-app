e1ebc0932acface3017e80794ae5fed1289821ec
LightRider5 <christoph@fulmo.org>
2024-11-29 16:49:04 +0100

    changed metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     template: "%s | LNVPN",
   },
   description:
-    "LNVPN offers a secure, no-log VPN service accepting Bitcoin payments for ultimate anonymity. Enhance your online privacy with our Bitcoin-only VPN solution.",
+    "Secure your privacy with LNVPN's no-log VPN. Pay with Bitcoin for ultimate anonymity. Plans start at just 10 cents! Get protected today.",
   keywords: [
     "Bitcoin VPN service",
     "anonymous VPN",
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     url: "https://lnvpn.net/",
     title: "Bitcoin VPN Service | LNVPN - Privacy-Focused VPN",
     description:
-      "LNVPN offers a secure, no-log VPN service accepting Bitcoin payments for ultimate anonymity. Enhance your online privacy with our Bitcoin VPN solution.",
+      "Secure your privacy with LNVPN's no-log VPN. Pay with Bitcoin for ultimate anonymity. Plans start at just 10 cents! Get protected today.",
     images: [{ url: "/LNVPN-Mask-Logo.svg" }],
   },
   metadataBase: new URL("https://lnvpn.net"),
@@ -51,7 +51,7 @@ export const metadata: Metadata = {
     creator: "@ln_vpn",
     title: "Bitcoin VPN Service | LNVPN - Privacy-Focused VPN",
     description:
-      "LNVPN offers a secure, no-log VPN service accepting Bitcoin payments for ultimate anonymity. Enhance your online privacy with our Bitcoin VPN solution.",
+      "Secure your privacy with LNVPN's no-log VPN. Pay with Bitcoin for ultimate anonymity. Plans start at just 10 cents! Get protected today.",
     images: ["/LNVPN-Mask-Logo.svg"],
   },
 };
