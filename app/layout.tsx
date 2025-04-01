2369168b916a5b6cac0741f5961a113a94be24c2
lnvpn <info@lnvpn.com>
2025-04-01 16:00:12 +0200

    added viewport for chrome mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -142,10 +142,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" className={inter.className}>
         <head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="icon" href="/icon.svg" type="image/svg+xml" />
           <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
