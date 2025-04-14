4d8e65778439cddeb8491ca71c269e18efc0b9b0
lnvpn <info@lnvpn.com>
2025-04-14 15:58:44 +0200

    changed security headers, whitelisted alby embed

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,27 @@ const nextConfig = {
             },
         ];
     },
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors https://my.albyhub.com;"
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin'
+                    }
+                ]
+            }
+        ];
+    }
 };
 
 export default nextConfig;
